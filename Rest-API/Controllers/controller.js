6a85db01b16d4347ecaa6dcb533e534c5803b6a5
galangrs <galang@samdues.com>
2023-12-10 23:44:52 +0700

    value error fixing

--- Rest-API/Controllers/controller.js
+++ Rest-API/Controllers/controller.js
@@ -121,7 +121,7 @@ class Controller {
             });
             if (!response) throw {name:"InvalidDelBal",message:"account not register"}
 
-            if (response.balance - count < 0 ) throw { name: "InvalidDelBal", message: `balance now ${response.balance} you cannot deduct in ${count}` };
+            if (response.balance - count < 0 ) throw { name: "InvalidDelBal", message: `you balance low cannot to deduct` };
 
             await User.update(
                 {
